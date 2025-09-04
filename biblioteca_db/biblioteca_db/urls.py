e90b4a94be8082a7615e4399444c12bff7ed0504
lauracast000009 <lauracatagomez46@gmail.com>
2025-09-04 15:47:24 -0500

    corecciones url

--- biblioteca_db/biblioteca_db/urls.py
+++ biblioteca_db/biblioteca_db/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from biblioteca_app.views import AutorViewSet, EditorialViewSet, LibroViewSet, MiembroViewSet, PrestamoViewSet
+
+router = DefaultRouter()
+router.register(r'autores', AutorViewSet)
+router.register(r'editoriales', EditorialViewSet)
+router.register(r'libros', LibroViewSet)
+router.register(r'miembros', MiembroViewSet)
+router.register(r'prestamos', PrestamoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),   # 👈 importante
 ]
