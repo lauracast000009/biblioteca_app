17f7705dad74a455e63469801e81b5856fca0389
lauracast000009 <lauracatagomez46@gmail.com>
2025-09-07 11:01:55 -0500

    Configuración de urls.py del proyecto principal

--- biblioteca_db/biblioteca_db/urls.py
+++ biblioteca_db/biblioteca_db/urls.py
@@ -16,17 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from biblioteca_app.views import AutorViewSet, EditorialViewSet, LibroViewSet, MiembroViewSet, PrestamoViewSet
-
-router = DefaultRouter()
-router.register(r'autores', AutorViewSet)
-router.register(r'editoriales', EditorialViewSet)
-router.register(r'libros', LibroViewSet)
-router.register(r'miembros', MiembroViewSet)
-router.register(r'prestamos', PrestamoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),   # 👈 importante
+    path('api/', include('biblioteca_app.urls')),  # 👈 solo incluyes la app
 ]
